7cd0b2e08d73a1f64eb2d0c33596363029354aa1
msolorio <msolorio789@gmail.com>
2016-01-13 00:34:11 -0800

    add fuctionality for comments dropdown

--- js/main.js
+++ js/main.js
@@ -14,3 +14,28 @@ $('.vacation-wrap').on('keyup', '.quantity', function(){
 	//update total
 	thisVacation.find('.total').html(total);
 });
+
+$('.vacation-wrap').on('click', '.view-comments', function(){
+	
+	//find the active comment
+	var activeComments = $('.comments.active');
+
+	//find clicked comments section
+	var thisComments = $(this).closest('.vacation-container').find('.comments');
+
+	function showNextComments() {
+		thisComments.slideDown(200, function(){
+			thisComments.addClass('active');
+		});
+	}
+
+	if(activeComments.length) {
+		activeComments.slideUp(200, function(){
+			activeComments.removeClass('active');
+			showNextComments();
+		});
+	} else {
+		showNextComments();
+		thisComments.addClass('active');
+	}
+});
